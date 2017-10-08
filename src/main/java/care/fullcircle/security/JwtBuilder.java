b90500583db77efb5259b18ea55c5ea7e8dda56b
kuprovskyi <oleh.kuprovskyi@cellsavers.com>
2017-10-08 18:49:46 +0300

    Jwt builder added

---
+++ src/main/java/care/fullcircle/security/JwtBuilder.java
@@ -0,0 +1,48 @@
+package care.fullcircle.security;
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by oleh.kuprovskyi on 05.10.17.
+ */
+public class JwtBuilder {
+
+    @Value("${jwt.header:'telemed'}")
+    private String tokenHeader;
+
+    public String createJWT(String username, String accountId, String role) {
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(now);
+        calendar.add(Calendar.HOUR, 1);
+
+        String jws = null;
+
+        try {
+            jws = Jwts.builder()
+                .setIssuer("Telemed")
+                .claim("username", username)
+                .claim("account_id", accountId)
+                .claim("role", role)
+                .setIssuedAt(now)
+                .setExpiration(calendar.getTime())
+                .signWith(
+                    SignatureAlgorithm.HS256,
+                    "telemed".getBytes("UTF-8")
+                )
+                .compact();
+        } catch (Exception e) {
+
+        }
+
+        return jws;
+    }
+}
